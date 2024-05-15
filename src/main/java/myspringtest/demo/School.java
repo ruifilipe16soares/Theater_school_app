80a1e146b4e12d9396ea7e6ee6710c050d4d0565
Francisco Reis <freis2002@gmail.com>
2024-05-15 12:19:50 +0100

    SDTP

---
+++ src/main/java/myspringtest/demo/School.java
@@ -0,0 +1,29 @@
+package myspringtest.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+    private List<User> users;
+
+    public School() {
+        this.users = new ArrayList<>();
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    //toString users
+    public String toString() {
+        String result = "";
+        for (User user : users) {
+            result += user.toString() + "\n";
+        }
+        return result;
+    }
+}
---
+++ src/main/java/myspringtest/demo/controller/WebController.java
@@ -0,0 +1,61 @@
+package myspringtest.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import myspringtest.demo.School;
+import myspringtest.demo.User;
+
+@Controller
+public class WebController {
+
+    private final School school;
+
+    public WebController() {
+        this.school = new School();
+        this.school.addUser(new User("admin", 0, 0, "admin@admin", "admin"));
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String email, @RequestParam String password) {
+        for (User user : school.getUsers()) {
+            System.out.println(school.toString());
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                if (user.getUserType() == 0) {
+                    return new ModelAndView("redirect:/admin");
+                } else if (user.getUserType() == 1) {
+                    return new ModelAndView("redirect:/student");
+                }
+            }
+        }
+        return new ModelAndView("login");
+    }
+
+    @GetMapping("/student")
+    public String student() {
+        return "student";
+    }
+
+    @GetMapping("/admin")
+    public ModelAndView admin() {
+        ModelAndView modelAndView = new ModelAndView("admin");
+        modelAndView.addObject("users", school.getUsers());
+        return modelAndView;
+    }
+
+    
+
+}
